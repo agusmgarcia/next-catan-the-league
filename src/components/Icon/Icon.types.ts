5028480f3b819e4183e52f31d5bb5896e26d5fbf
Agus Garcia <agusss.garcia19@gmail.com>
2026-01-06 15:59:01 -0300

    feat(Icon): add add variant

--- src/components/Icon/Icon.types.ts
+++ src/components/Icon/Icon.types.ts
@@ -1,5 +1,5 @@
 type IconProps = Pick<React.SVGProps<SVGSVGElement>, "className"> & {
-  variant: "crown" | "google" | "medal" | "spinner";
+  variant: "add" | "crown" | "google" | "medal" | "spinner";
 };
 
 export default IconProps;
