d2dd8e3f6571787dfe095e07804809220dd08ceb
Agus Garcia <agusss.garcia19@gmail.com>
2026-01-09 16:09:45 -0300

    feat(Button): add primary and secondary variant

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,6 +6,34 @@ import type ButtonProps from "./Button.types";
 export default function Button(props: ButtonProps) {
   const { children, className, variant, ...rest } = useButton(props);
 
+  if (!variant || variant === "primary")
+    return (
+      <button
+        {...rest}
+        className={twMerge(
+          "h-14 w-full cursor-pointer rounded-lg border border-interface-red bg-interface-red p-4 text-white transition-colors",
+          "disabled:cursor-default disabled:border-gray-500 disabled:bg-gray-500",
+          className,
+        )}
+      >
+        {children}
+      </button>
+    );
+
+  if (variant === "secondary")
+    return (
+      <button
+        {...rest}
+        className={twMerge(
+          "h-14 w-full cursor-pointer rounded-lg border bg-white p-4 text-black transition-colors",
+          "disabled:cursor-default disabled:border-gray-300 disabled:bg-gray-300 disabled:text-gray-500",
+          className,
+        )}
+      >
+        {children}
+      </button>
+    );
+
   if (variant === "raw")
     return (
       <button
--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -2,7 +2,7 @@ type ButtonProps = Pick<
   React.ButtonHTMLAttributes<HTMLButtonElement>,
   "children" | "className" | "disabled" | "onClick" | "type"
 > & {
-  variant: "raw";
+  variant?: "primary" | "raw" | "secondary";
 };
 
 export default ButtonProps;
