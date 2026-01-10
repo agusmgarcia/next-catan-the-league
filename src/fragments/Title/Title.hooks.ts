a6eb8edbfdc94b071abdff02a324730c46141201
Agus Garcia <agusss.garcia19@gmail.com>
2026-01-10 16:34:23 -0300

    chore(Title): rename prop inside hook

--- src/fragments/Title/Title.hooks.ts
+++ src/fragments/Title/Title.hooks.ts
@@ -2,11 +2,14 @@ import { useMemo } from "react";
 
 import type TitleProps from "./Title.types";
 
-export default function useTitle({ children, ...rest }: TitleProps) {
+export default function useTitle({
+  children: childrenFromProps,
+  ...rest
+}: TitleProps) {
   const title = useMemo(
     () =>
-      `Catan - The League${Array.isArray(children) ? (!!children.length ? ` :: ${children.join(" :: ")}` : "") : !!children ? ` :: ${children}` : ""}`,
-    [children],
+      `Catan - The League${Array.isArray(childrenFromProps) ? (!!childrenFromProps.length ? ` :: ${childrenFromProps.join(" :: ")}` : "") : !!childrenFromProps ? ` :: ${childrenFromProps}` : ""}`,
+    [childrenFromProps],
   );
 
   return { ...rest, title };
