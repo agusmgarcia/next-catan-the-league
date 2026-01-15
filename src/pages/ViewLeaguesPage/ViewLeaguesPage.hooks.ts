e6b27fe15392a5ef875cf5c891a926a9e0e27ffd
Agus Garcia <agusss.garcia19@gmail.com>
2026-01-15 09:45:44 -0300

    feat(ViewLeaguesPage): implement page

--- src/pages/ViewLeaguesPage/ViewLeaguesPage.hooks.ts
+++ src/pages/ViewLeaguesPage/ViewLeaguesPage.hooks.ts
@@ -1,5 +1,69 @@
+import { sorts, strings } from "@agusmgarcia/react-essentials-utils";
+import { useMemo } from "react";
+
+import {
+  useLeague,
+  useLeagues,
+  useMatches,
+  type Users,
+  useUsers,
+} from "#src/store";
+
 import type ViewLeaguesPageProps from "./ViewLeaguesPage.types";
 
 export default function useViewLeaguesPage(props: ViewLeaguesPageProps) {
-  return { ...props };
+  const { league } = useLeague();
+  const { users: usersFromStore } = useUsers();
+  const { leagues: leaguesFromStore } = useLeagues();
+  const { matches: matchesFromStore } = useMatches();
+
+  const leagues = useMemo(
+    () =>
+      leaguesFromStore
+        .map((l) => {
+          const users = usersFromStore.reduce(
+            (result, user) => {
+              result[user.id] = user;
+              return result;
+            },
+            {} as Record<string, Users[number]>,
+          );
+
+          const matches = matchesFromStore
+            .filter((m) => m.leagueId === l.id)
+            .filter((m) => m.players.every((p) => !!p.approved));
+
+          return {
+            active: l.id === league?.id,
+            id: l.id,
+            matchesCount: strings.replace(
+              "${matchesLength} of ${totalMatches} ${totalMatches?match:matches}",
+              { matchesLength: matches.length, totalMatches: l.matchesCount },
+            ),
+            name: l.name,
+            players: l.players
+              .map((p) => ({
+                color: p.color,
+                id: p.id,
+                photoURL: users[p.id]?.photoURL,
+                points: matches
+                  .map(
+                    (m) => m.players.find((mp) => mp.id === p.id)?.points || 0,
+                  )
+                  .reduce((result, points) => result + points, 0),
+                victoryCounts: matches.filter((m) => m.winnerId === p.id)
+                  .length,
+              }))
+              .sort((p1, p2) => sorts.byNumberDesc(p1.points, p2.points))
+              .sort((p1, p2) =>
+                sorts.byNumberDesc(p1.victoryCounts, p2.victoryCounts),
+              ),
+            updatedAt: l.updatedAt,
+          };
+        })
+        .sort((l1, l2) => sorts.byNumberDesc(l1.updatedAt, l2.updatedAt)),
+    [league?.id, leaguesFromStore, matchesFromStore, usersFromStore],
+  );
+
+  return { ...props, leagues };
 }
--- src/pages/ViewLeaguesPage/ViewLeaguesPage.tsx
+++ src/pages/ViewLeaguesPage/ViewLeaguesPage.tsx
@@ -1,8 +1,69 @@
+import { twMerge } from "tailwind-merge";
+
+import { Anchor, Button, Icon, PlayerImage, Typography } from "#src/components";
+
 import useViewLeaguesPage from "./ViewLeaguesPage.hooks";
 import type ViewLeaguesPageProps from "./ViewLeaguesPage.types";
 
 export default function ViewLeaguesPage(props: ViewLeaguesPageProps) {
-  const { ...rest } = useViewLeaguesPage(props);
+  const { leagues, ...rest } = useViewLeaguesPage(props);
 
-  return <div {...rest} />;
+  return (
+    <div {...rest} className="flex size-full flex-col gap-4 overflow-auto">
+      {/* LEAGUES */}
+      {leagues.map((l) => (
+        <div
+          key={l.id}
+          className={twMerge(
+            "flex flex-col gap-4 rounded-lg border-4 bg-white/60 custom-noise-5 p-4 shadow-2xl",
+            l.active && "border-interface-green",
+          )}
+        >
+          {/* HEADER */}
+          <div className="flex items-center justify-between gap-1">
+            {/* NAME */}
+            <Typography className="underline" variant="h2">
+              {l.name}
+            </Typography>
+
+            {/* MATCHES COUNT */}
+            <Typography className="rounded-lg bg-interface-yellow px-2">
+              {l.matchesCount}
+            </Typography>
+          </div>
+
+          <div className="flex items-center justify-between gap-2">
+            {/* PLAYERS */}
+            <div className="flex gap-2">
+              {l.players.map((p) => (
+                <div key={p.id} className="flex flex-col gap-1">
+                  {/* PLAYER IMAGE */}
+                  <PlayerImage
+                    color={p.color}
+                    src={p.photoURL}
+                    variant="3rem"
+                  />
+
+                  {/* VICTORY POINTS */}
+                  <Typography className="w-12 text-center">
+                    {p.points}
+                  </Typography>
+                </div>
+              ))}
+            </div>
+
+            {/* VIEW LEAGUE */}
+            <Anchor href={`/leagues/${l.id}/view`}>
+              <Icon className="size-10" variant="arrowRightWide" />
+            </Anchor>
+          </div>
+        </div>
+      ))}
+
+      {/* CREATE NEW LEAGUE */}
+      <Anchor href="/leagues/create">
+        <Button variant="primary">Create new league</Button>
+      </Anchor>
+    </div>
+  );
 }
