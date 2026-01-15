ae14666aa59c011819272048262792367e3d0295
Agus Garcia <agusss.garcia19@gmail.com>
2026-01-15 09:09:45 -0300

    fix(PlayerImage): make color required

--- src/components/PlayerImage/PlayerImage.types.ts
+++ src/components/PlayerImage/PlayerImage.types.ts
@@ -1,6 +1,6 @@
 type PlayerImageProps = {
   alt?: string;
-  color?: "blue" | "brown" | "green" | "orange" | "red" | "white";
+  color: "blue" | "brown" | "green" | "orange" | "red" | "white" | undefined;
   src: string | undefined;
   variant: "3.5rem" | "3rem";
 };
