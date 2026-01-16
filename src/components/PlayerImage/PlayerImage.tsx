1213b9af88a90f316b1528f5d9ddec77eb1198e2
Agus Garcia <agusss.garcia19@gmail.com>
2026-01-16 08:31:57 -0300

    feat(PlayerImage): add 6rem variant

--- src/components/PlayerImage/PlayerImage.tsx
+++ src/components/PlayerImage/PlayerImage.tsx
@@ -14,7 +14,9 @@ export default function PlayerImage(props: PlayerImageProps) {
       className={twMerge(
         variant === "3rem"
           ? "size-12 max-h-12 min-h-12 max-w-12 min-w-12"
-          : "size-14 max-h-14 min-h-14 max-w-14 min-w-14",
+          : variant === "3.5rem"
+            ? "size-14 max-h-14 min-h-14 max-w-14 min-w-14"
+            : "size-20 max-h-20 min-h-20 max-w-20 min-w-20",
         "rounded-full border-4",
         color === "red"
           ? "border-player-red"
--- src/components/PlayerImage/PlayerImage.types.ts
+++ src/components/PlayerImage/PlayerImage.types.ts
@@ -2,7 +2,7 @@ type PlayerImageProps = {
   alt?: string;
   color: "blue" | "brown" | "green" | "orange" | "red" | "white" | undefined;
   src: string | undefined;
-  variant: "3.5rem" | "3rem";
+  variant: "3.5rem" | "3rem" | "6rem";
 };
 
 export default PlayerImageProps;
